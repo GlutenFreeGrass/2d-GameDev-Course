f8a8a2f2e6d136d91f0326d7974e73eea81c7834
GlutenFreeGrass <kawu11@s.sfusd.edu>
2024-03-29 15:22:16 -0700

    L3-Make-The-Ship-Interact

--- 05.loot_it_all_workbook/lessons/area_2d.gd
+++ 05.loot_it_all_workbook/lessons/area_2d.gd
@@ -3,13 +3,13 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func ready():
-	return
+	area_entered.connect(_on_area_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _on_area_entered(area):
+func _on_area_entered(_on_area_entered: Area2D) -> void:
 	queue_free()
 
