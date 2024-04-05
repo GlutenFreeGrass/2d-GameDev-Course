4d3f4d8810cb3f0b4aaa09af88fddc4a6d77147e
GlutenFreeGrass <kawu11@s.sfusd.edu>
2024-04-05 15:41:21 -0700

    L6-Add-Collectible-Gems

--- 05.loot_it_all_workbook/lessons/ship.gd
+++ 05.loot_it_all_workbook/lessons/ship.gd
@@ -4,6 +4,7 @@ var health := 10
 var max_speed := 1200.0
 var velocity := Vector2(0, 0)
 var steering_factor := 3.0
+var gem_count := 0
 
 func _ready():
 	set_health(50)
@@ -29,5 +30,12 @@ func set_health(new_health: int) -> void:
 	health = new_health
 	get_node("UI/HealthBar").value = health
 	
+func set_gem_count(new_gem_count: int) -> void:
+	gem_count = new_gem_count
+	get_node("UI/GemCount").text = "x" + str(gem_count)
+	
 func _on_area_entered(area_that_entered: Area2D) -> void:
-	set_health(health + 10)
+	if area_that_entered.is_in_group("healing_item"):
+		set_health(health + 10)
+	elif area_that_entered.is_in_group("gem"):
+		set_gem_count(gem_count + 1)
