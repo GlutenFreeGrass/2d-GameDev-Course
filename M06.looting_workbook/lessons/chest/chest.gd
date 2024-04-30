958b97b6d65fa21390741184b83d252d2aaa41e3
GlutenFreeGrass <kawu11@s.sfusd.edu>
2024-04-30 13:55:52 -0700

    L6-Spawn-Random-Loot

--- M06.looting_workbook/lessons/chest/chest.gd
+++ M06.looting_workbook/lessons/chest/chest.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var possible_items: Array[PackedScene] = []
 @onready var canvas_group: CanvasGroup = $CanvasGroup
 @onready var animation_player: AnimationPlayer = $AnimationPlayer 
 
@@ -7,6 +8,10 @@ extends Area2D
 func open() -> void:
 	animation_player.play("open")
 	input_pickable = false
+	if possible_items.is_empty():
+		return
+	for current_index in range(randi_range(1, 3)):
+		_spawn_random_item()
 	
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
@@ -33,3 +38,11 @@ func _input_event(viewport: Node, event: InputEvent, shape_idx: int):
 	var event_is_mouse_click: bool = (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed())
 	if event_is_mouse_click:
 		open()
+		
+func _spawn_random_item() -> void:
+	var loot_item: Area2D = possible_items.pick_random().instantiate()
+	add_child(loot_item)
+	var random_angle := randf_range(0.0, 2.0 * PI)
+	var random_direction := Vector2(0.75, 0.0).rotated(random_angle)
+	var random_distance := randf_range(60.0, 120.0)
+	loot_item.position = random_direction * random_distance
