5419ec321d1a6b53ae1689cfabc859f2f8d40f79
GlutenFreeGrass <kawu11@s.sfusd.edu>
2024-12-19 08:32:03 -0800

    L9-L10
    Incomplete but wtv

--- M09.top_down_movement_workbook/game.gd
+++ M09.top_down_movement_workbook/game.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var runner = $Runner
+@onready var count_down = %CountDown
 @onready var finish_line = %FinishLine
 
 # Called when the node enters the scene tree for the first time.
@@ -20,7 +22,12 @@ func _ready():
 			finish_line.pop_confettis
 		)
 	)
-
+	count_down.start_counting()
+	runner.set_physics_process(false)
+	count_down.counting_finished.connect(
+		func() -> void:
+			runner.set_physics_process(true)
+	)
 	finish_line.confettis_finished.connect(
 		get_tree().reload_current_scene
 	)
